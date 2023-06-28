cad669b9e1ed10976f9c6198e84e74a2f77a7452
mikumifa <1055069518@qq.com>
2023-06-28 21:39:39 +0800

    lab6

--- backend-lab6/src/main/java/org/fffd/l23o6/dao/RouteDao.java
+++
@@ -1,8 +0,0 @@
-package org.fffd.l23o6.dao;
-
-import org.fffd.l23o6.pojo.entity.RouteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RouteDao extends JpaRepository<RouteEntity, Long>{
-    
-}

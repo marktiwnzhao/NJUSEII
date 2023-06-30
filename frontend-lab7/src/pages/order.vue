82023c3d0c4b3dafd85ab43b8071ed7ce5418e0a
mikumifa <1055069518@qq.com>
2023-06-30 12:08:06 +0800

    修改了order导航项的bug

--- frontend-lab7/src/pages/order.vue
+++ frontend-lab7/src/pages/order.vue
@@ -3,6 +3,7 @@
 import { useRoute } from "vue-router";
 
 const route = useRoute()
+const hasOrder = typeof route.params['orderId'] === 'string';
 
 </script>
 
@@ -12,17 +13,22 @@ const route = useRoute()
       <MenuComponent pageIndex="/order" />
     </el-header>
     <el-main style="margin-top: 10vh">
-      <div>
+      <div v-if="hasOrder">
         <el-text size="large" type="primary" style="display: flex;justify-content: center">
           <h1>订单详情</h1>
         </el-text>
         <br />
-      </div>
 
 
       <div style="display: flex; justify-content: center; margin-top: 5vh">
         <OrderDetail :id="typeof $route.params['orderId'] === 'string' ? parseInt($route.params['orderId']) : undefined"
           style="width: 50%" />
+      </div>
+      </div>
+      <div v-else>
+        <el-text size="medium" type="info" style="display: flex; justify-content: center">
+          <h3>当前没有订单</h3>
+        </el-text>
       </div>
     </el-main>
   </el-container>

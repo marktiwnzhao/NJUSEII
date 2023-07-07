4cc82d0e674f05a013a82e43ff2c1a001a509f25
mikumifa <1055069518@qq.com>
2023-07-07 17:52:45 +0800

    补充注释

--- backend-lab7/src/main/java/org/fffd/l23o6/service/impl/RouteServiceImpl.java
+++ backend-lab7/src/main/java/org/fffd/l23o6/service/impl/RouteServiceImpl.java
@@ -17,28 +17,61 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RouteServiceImpl implements RouteService {
     private final RouteDao routeDao;
+
+    /**
+     * 添加路线
+     *
+     * @param name       路线名称
+     * @param stationIds 站点ID列表
+     */
     @Override
     public void addRoute(String name, List<Long> stationIds) {
         RouteEntity route = RouteEntity.builder().name(name).stationIds(stationIds).build();
         routeDao.save(route);
     }
 
+    /**
+     * 获取路线列表
+     *
+     * @return 路线VO列表
+     */
     @Override
     public List<RouteVO> listRoutes() {
-        return routeDao.findAll(Sort.by(Sort.Direction.ASC, "name")).stream().map(RouteMapper.INSTANCE::toRouteVO).collect(Collectors.toList());
+        return routeDao.findAll(Sort.by(Sort.Direction.ASC, "name"))
+                .stream()
+                .map(RouteMapper.INSTANCE::toRouteVO)
+                .collect(Collectors.toList());
     }
 
+    /**
+     * 根据ID获取路线详情
+     *
+     * @param id 路线ID
+     * @return 路线VO
+     */
     @Override
     public RouteVO getRoute(Long id) {
         RouteEntity entity = routeDao.findById(id).get();
         return RouteMapper.INSTANCE.toRouteVO(entity);
     }
 
+    /**
+     * 编辑路线
+     *
+     * @param id         路线ID
+     * @param name       路线名称
+     * @param stationIds 站点ID列表
+     */
     @Override
     public void editRoute(Long id, String name, List<Long> stationIds) {
         routeDao.save(routeDao.findById(id).get().setStationIds(stationIds).setName(name));
     }
 
+    /**
+     * 删除路线
+     *
+     * @param routeId 路线ID
+     */
     @Override
     public void delRoute(Long routeId) {
         routeDao.deleteById(routeId);
